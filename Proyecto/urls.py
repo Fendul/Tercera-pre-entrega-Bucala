78ee5e36c34d3f7b6a0cce77a66bfd0b199a21e5
MBucala <118484101+Fendul@users.noreply.github.com>
2024-08-27 21:35:59 -0300

    Update urls.py

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("appmuebleria/", include ("appmuebleria.urls")),
+    path("", include ("appmuebleria.urls")),
    
 ]
